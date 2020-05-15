7301418026fbf4cafa45fab01487147675ade58e
Jane Elizabeth Gómez Corrales <janegomezc@gmail.com>
2020-05-14 22:55:16 -0600

    Modificando a SQLite

--- app/src/main/java/com/rockbass/contactos/ContactosContract.java
+++ app/src/main/java/com/rockbass/contactos/ContactosContract.java
@@ -1,4 +1,19 @@
 package com.rockbass.contactos;
 
+import android.provider.BaseColumns;
+
 public class ContactosContract {
+
+    public ContactosContract(){}
+
+    public static class ContactoEntry implements BaseColumns{
+        public static final String NOMBRE_TABLA = "Contactos";
+        public static final String COLUMNA_NOMBRE = "nombre";
+        public static final String COLUMNA_APELLIDO_PATERNO = "apellidoPaterno";
+        public static final String COLUMNA_APELLIDO_MATERNO = "apellidoMaterno";
+        public static final String COLUMNA_TELEFONO = "telefono";
+        public static final String COLUMNA_EDAD = "edad";
+        public static final String COLUMNA_EMAIL = "email";
+        public static final String COLUMNA_CONTACTO = "contacto";
+    }
 }
